aa9a42fd439456c3fced612b9d1c02dd208a3d12
Lasillya <divna1999@gmail.com>
2025-02-16 16:25:48 +0100

    Adding controller for model

---
+++ SuperHerose/Controllers/SuperHeroController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHerose.Models;
+
+namespace SuperHerose.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllHerose()
+        {
+            var herose = new List<SuperHero> {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Spiderman",
+                    FirstName = "Piter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+
+            return Ok(herose);
+        }
+    }
+}
